cf78f00087ca1be57ae17fe60163a0565652e275
Jonathan Xu <s014290@students.lmsd.org>
2021-04-06 07:49:19 -0400

    Initial Commit

---
+++ Translate AI Host App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Translate AI Host App
+//
+//  Created by Jonathan Xu (student LM) on 4/6/21.
+//  Copyright © 2021 Jonathan Xu (student LM). All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Translate AI Host AppTests/Translate_AI_Host_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Translate_AI_Host_AppTests.swift
+//  Translate AI Host AppTests
+//
+//  Created by Jonathan Xu (student LM) on 4/6/21.
+//  Copyright © 2021 Jonathan Xu (student LM). All rights reserved.
+//
+
+import XCTest
+@testable import Translate_AI_Host_App
+
+class Translate_AI_Host_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
